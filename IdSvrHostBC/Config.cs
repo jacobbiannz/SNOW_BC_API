d4982f3756f8db1e8dbf65acf008c6207eb6bf6d
boChuck <bo.rubyxing@gmail.com>
2018-05-27 00:00:16 +1200

    id server

---
+++ IdSvrHostBC/Config.cs
@@ -0,0 +1,86 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdSvrHostBC
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                 new ApiResource("api1", "My API")
+            };
+        }
+
+        public static List<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(), // <-- usefull
+                new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    RequireConsent = false,
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+
+                new Client {
+                    ClientId = "angular_spa",
+                    ClientName = "Angular 4 Client",
+                    RequireConsent = false,
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = new List<string> { "openid", "profile", "api1", "email"},
+                    RedirectUris = new List<string> { "http://localhost:31155/auth-callback" },
+                    PostLogoutRedirectUris = new List<string> { "http://localhost:31155/index.html" },
+                    AllowedCorsOrigins = new List<string> { "http://localhost:31155" },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+        }
+
+        /*
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+             {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+        }
+        */
+
+    }
+}
